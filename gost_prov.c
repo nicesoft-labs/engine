cf25896245b5f3dc4975dc481441d6da6e2f0b42
nicesoft-labs <info@ncsgp.ru>
2025-07-06 13:40:21 +0300

    Update gost_prov.c

--- gost_prov.c
+++ gost_prov.c
@@ -98,6 +98,8 @@ static const OSSL_ALGORITHM *gost_operation(void *vprovctx,
         return GOST_prov_keymgmts;
     case OSSL_OP_ENCODER:
         return GOST_prov_encoders;
+    case OSSL_OP_DECODER:
+        return GOST_prov_decoders;
     }
     return NULL;
 }
